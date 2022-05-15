032848ef60e626b4ae1af8bc1b880a34fed972bb
LUPOSaymon <simone.mija@gmail.com>
2022-05-15 14:15:55 +0200

    Initial Commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#Create an array where the array[index] contains the factorial of index
+def createFactorialArray(z):
+    return None
+
+#With a grid n*n, count all possible paths with at least k turns
+def countPaths(n,k):
+    factorials = createFactorialArray(n-2)
