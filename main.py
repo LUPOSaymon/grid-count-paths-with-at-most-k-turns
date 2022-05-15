802991cb870ec86f8084754d6561d89e06732ae6
LUPOSaymon <simone.mija@gmail.com>
2022-05-15 14:39:34 +0200

    Added createFactorialArray function

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-#Create an array where the array[index] contains the factorial of index
-def createFactorialArray(z):
-    return None
+# Create an array where the array[index] contains the factorial of index-1
+def createFactorialArray(z):#O(n)
+    factorials = [1] * (z) #(z+1 cause we include
+    for i in range(1, z):
+        factorials[i] = factorials[i - 1] * (i+1)
+    return factorials
 
-#With a grid n*n, count all possible paths with at least k turns
 def countPaths(n,k):
-    factorials = createFactorialArray(n-2)
+    return None
+# With a grid n*n, count all possible paths with at least k turns
+def printPaths(n, k):
+    factorials = createFactorialArray(n - 2)
+    count = countPaths(n,k)
+    print(factorials)
+
+
