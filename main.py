62081957b65b1bcbc604cd3d5f3bbac2a3ea9951
LUPOSaymon <simone.mija@gmail.com>
2022-05-16 17:49:18 +0200

    Added printChart2 function

--- main.py
+++ main.py
@@ -3,6 +3,72 @@ import math
 import time
 import matplotlib.pyplot as plt
 
+
+
+
+# Returns count of paths to reach
+# (i, j) from (0, 0) using at-most k turns.
+# d is current direction, d = 0 indicates
+# along row, d = 1 indicates along column.
+def countPathsUtil(i, j, k, d,dp):
+    # If invalid row or column indexes
+    if (i < 0 or j < 0):
+        return 0
+
+    # If current cell is top left itself
+    if (i == 0 and j == 0):
+        return 1
+
+    # If 0 turns left
+    if (k == 0):
+
+        # If direction is row, then we can reach here
+        # only if direction is row and row is 0.
+        if (d == 0 and i == 0):
+            return 1
+
+        # If direction is column, then we can reach here
+        # only if direction is column and column is 0.
+        if (d == 1 and j == 0):
+            return 1
+
+        return 0
+
+    # If this subproblem is already evaluated
+    if (dp[i][j][k][d] != -1):
+        return dp[i][j][k][d]
+
+    # If current direction is row,
+    # then count paths for two cases
+    # 1) We reach here through previous row.
+    # 2) We reach here through previous column,
+    # so number of turns k reduce by 1.
+    if (d == 0):
+        dp[i][j][k][d] = countPathsUtil(i, j - 1, k, d,dp) + \
+                         countPathsUtil(i - 1, j, k - 1, not d,dp)
+        return dp[i][j][k][d]
+
+    # Similar to above if direction is column
+    dp[i][j][k][d] = countPathsUtil(i - 1, j, k, d,dp) + \
+                     countPathsUtil(i, j - 1, k - 1, not d,dp)
+    return dp[i][j][k][d]
+
+
+# This function mainly initializes 'dp' array
+# as -1 and calls countPathsUtil()
+def countPaths2(i, j, k,MAX):
+    # If (0, 0) is target itself
+    if (i == 0 and j == 0):
+        return 1
+    dp = [[[[-1 for col in range(2)]
+            for col in range(MAX+1)]
+           for row in range(MAX+1)]
+          for row in range(MAX+1)]
+    # Recur for two cases: moving along row
+    # and along column
+    return countPathsUtil(i - 1, j, k, 1,dp) + \
+           countPathsUtil(i, j - 1, k, 0,dp)
+
 def createFactorialArray(z):  # O(n)
     factorials = [1] * (z+1)
     for i in range(1, z+1):
@@ -27,27 +93,35 @@ def printPaths(n, k):
 
 
 
-n= 35
-k=11
-printPaths(n,k)
+
 
 
 def printChart():
     k = 4
     times = list()
     elements = list()
-    for i in range(k,10000):
+    times2 = list()
+    elements2=list()
+    for i in range(k,200):
         n = i
-        start = time.process_time()
+        start = time.perf_counter()
         factorials = createFactorialArray(n - 2)
         countPaths(n, k, factorials)
-        end = time.process_time()
+        end = time.perf_counter()
         elements.append(n)
         times.append(end-start)
+
+        start = time.perf_counter()
+        MAX = n
+        countPaths2(n-1,n-1,k,MAX)
+        end = time.perf_counter()
+        times2.append(end-start)
+        elements2.append(n)
 
     plt.xlabel('n')
     plt.ylabel('Time')
     plt.plot(elements, times, label='Actual Time')
+    plt.plot(elements2,times2,label="Count")
     plt.grid()
     plt.legend()
     plt.show()
